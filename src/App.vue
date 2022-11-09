f775c4f674902e192c25b9de65372b3fdcd977e5
Valiantsin Pratasevich <Valiantsin.Pratasevich@solbeg.com>
2022-11-09 20:23:17 +0300

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,77 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="app">
+    <h1>Страница с постами</h1>
+    <my-button style="margin: 15px 0;" @click="showDialog">Создать пост</my-button>
+    <my-dialog v-model:show="dialogVisible">
+      <PostForm
+        @create="createPost"
+      />
+    </my-dialog>
+    <PostList 
+      :posts="posts"
+      @remove="removePost"
+      v-if="!isPostsLoading"
+    />
+    <div v-else>Идет загрузка...</div>
+  </div>
 </template>
 
+<script>
+  import PostForm from '@/components/PostForm.vue';
+  import PostList from '@/components/PostList.vue';
+  import MyButton from './components/UI/MyButton.vue';
+  import axios from 'axios';
+
+  export default {
+    components: {
+      PostForm, PostList,
+        MyButton
+    },
+    data() {
+      return {
+        posts: [],
+        dialogVisible: false,
+        modificatorValue: '',
+        isPostsLoading: false,
+      }
+    },
+    methods: {
+      createPost(post) {
+        this.posts.push(post);
+        this.dialogVisible = false;
+      },
+      removePost(post) {
+        this.posts = this.posts.filter(p => p.id !== post.id)
+      },
+      showDialog() {
+        this.dialogVisible = true;
+      },
+      async fetchPosts() {
+        try {
+          this.isPostsLoading = true;
+          const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10');
+          this.posts = response.data;
+        } catch (e) {
+          alert('Ошибка')
+        } finally {
+          this.isPostsLoading = false;
+        }
+      }
+    },
+    mounted() {
+      this.fetchPosts();
+    }
+  }
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+  .app {
+    padding: 20px;
+  }
 </style>
---
+++ src/components/PostList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div v-if="posts.length > 0">
+    <h3>Список постов</h3>
+    <PostITem 
+      v-for="post in posts"
+      :post="post"
+      :key="post.id"
+      @remove="$emit('remove', post)"
+    />
+  </div>
+  <h2 v-else style="color: red">
+    Список постов пуст
+  </h2>
+</template>
+
+<script>
+import PostITem from './PostITem.vue';
+
+  export default {
+    components: { PostITem },
+    props: {
+        posts: {
+            type: Array,
+            required: true
+        }
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
